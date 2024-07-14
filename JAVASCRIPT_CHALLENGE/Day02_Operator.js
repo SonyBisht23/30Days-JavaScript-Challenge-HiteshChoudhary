363a81108bbb65306f8690f4abe1e910863320d8
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-07-14 17:10:17 +0000

    Operators

---
+++ JAVASCRIPT_CHALLENGE/Day02_Operator.js
@@ -0,0 +1,85 @@
+//********************* Arithmetic Operator *************************************
+// Write a program to "add" two numbers and log the result to the console.
+let num1=12;
+let num2=24;
+console.log("Sum =",num1+num2)
+//Write a program to "subtract" two numbers and log the result to the console.
+let num3=32;
+let num4=24;
+console.log("Subtraction =",num3-num4)
+// Write a program to "multiply" two numbers and log the result to the console.
+let num5=12;
+let num6=24;
+console.log("Mutiplication =",num1*num2)
+// Write a program to "divide" two numbers and log the result to the console.
+let num7=144;
+let num8=24;
+console.log("Division =",num7/num8)
+// Write a program to find the "remainder" when one number is divided by another and log the result to the console.
+let num9=124;
+let num10=24;
+console.log("Remainder =",num9%num10)
+
+//********************* Assignment Operator *************************************
+//Use the "+=" operator to "add" a number to a variable and log the result to the console.
+let x=10;
+console.log(x+=6);
+//Use the "-=" operator to "subtract" a number to a variable and log the result to the console.
+let y=10;
+console.log(y-=6);
+
+//********************* Comparison Operator ************************************
+//Write a program to compare two numbers using > and < and log the result to the console.
+let a=10,b=20;
+console.log("a>b:",a>b);
+console.log("a<b:",a<b);
+//Write a program to compare two numbers using >= and =< and log the result to the console.
+console.log("a>=b:",a>=b);
+console.log("a<=b:",a<=b);
+//Write a program to compare two numbers using == and === and log the result to the console.
+let c="10",d=10;
+console.log("c==d :",c==d);
+console.log("c===d: ",c===d);
+
+//********************* Logical Operator ************************************
+//Write a program that uses the && operator to combine two conditions and log the result to the console.
+let result=true;
+let marks=false;
+console.log(result && marks);
+//Write a program that uses the || operator to combine two conditions and log the result to the console.
+console.log(result || marks);
+//Write a program that uses ! operator to negate a condition and log the result to the console.
+console.log(!marks);
+
+//********************* Ternary Operator ************************************
+//Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let num=-10;
+ans=num>=0?"Positive":"Negative";
+console.log(ans);
+
+//********************* Arithmetic Operation Script ************************************
+/*Write a script that performs basic arithmetic operations (addition, subtraction, multiplication, division, remainder) 
+on two numbers and logs the result.*/
+let n1=24,n2=25;
+console.log("n1+n2 :",n1+n2);
+console.log("n1-n2 :",n1-n2);
+console.log("n1*n2 :",n1*n2);
+console.log("n1/n2 :",n1/n2);
+console.log("n1%n2 :",n1%n2);
+
+//********************* Comparison and Logical Operators Script ************************************
+/*Create a script that compares two numbers using different comparison operators and combines condition using logical 
+operators, logging the results. */
+let n3=23, n4="23";
+console.log("n3>n4 :",n3>n4);
+console.log("n3<n4 :",n3<n4);
+console.log("n3>=n4 :",n3>=n4);
+console.log("n3<=n4 :",n3<=n4);
+console.log("n3==n4 :",n3==n4);
+console.log("n3===n4 :",n3===n4);
+
+//********************* Teranry Operator Script ************************************
+//Write a script that uses the ternary operator to determine if a number is positive or negative and logs the result.
+let nu=-10;
+ans=nu>=0?"Positive":"Negative";
+console.log(ans);
