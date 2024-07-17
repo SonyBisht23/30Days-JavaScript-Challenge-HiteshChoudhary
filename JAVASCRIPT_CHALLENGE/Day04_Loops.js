408b2f17974bd10e19662b68cc3babb50d071f3c
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-07-17 22:49:28 +0000

    Loops

---
+++ JAVASCRIPT_CHALLENGE/Day04_Loops.js
@@ -0,0 +1,24 @@
+// ******************** For Loop ************************
+// Write a program to print numbers from 1 to 10.
+for (let num = 0; num <=10; num++) {
+   console.log(num);
+}
+// Write a program to print the multiplication table of 5.
+// ******************** While Loop ************************
+// Write a program to calculate the sum of numbers from 1 to 10.
+// Write a program to print numbers from 10 to 1.
+// ******************** Do - While Loop ************************
+// Write a program to print numbers from 1 to 5.
+// Write a program to calculate the factorial of a number.
+// ********************  Loop Control Statement ************************
+/*
+    Write a program to print a pattern.
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+*/
+// ******************** For Loop ************************
+// Write a program to print numbers from 1 to 10,but skip the number 5.
+// Write a program to print numbers from 1 to 10,but stop the loop when num is 7.
