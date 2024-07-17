737543a94695e370685e4e3c701a039e663590ea
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-07-17 23:48:05 +0000

    Function

---
+++ JAVASCRIPT_CHALLENGE/Day05_Functions.js
@@ -0,0 +1,15 @@
+// **************** Function Declaration *******************
+// Write a function to check if a number is even or odd.
+// Write a function to calculate the square of a number.
+// **************** Function Expression *******************
+// Write a function expression to find the maxium of two numbers.
+// Write a function expression to concatenate two strings.
+// **************** Arrow Function *******************
+// Write an arrow function to calculate the sum of two numbers.
+// Write an arroe function to check if a string contains a specific charachter and return a boolean value.
+// **************** Function Parameters and Default Values *******************
+// Write a function that takes a person's name and age and returns a greeting message.Provide a default value for the age.
+// Write a function that takes two parameters and returns their products.Provide a default value for the second parameter.
+// **************** Higher - Order Functions *******************
+// Write a higher - order function that takes a function and a number,and calls the function that many times.
+// Write a higher - order function that takes two functions and a value, applies the first function to the value, and then applies the second function .
